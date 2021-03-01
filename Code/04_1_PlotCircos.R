00bcd7aa2b8ff0edde67ead4cb3e3b4b758d0683
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2021-03-01 16:14:57 -0500

    improved quality of export plot

--- Code/04_1_PlotCircos.R
+++ Code/04_1_PlotCircos.R
@@ -8,7 +8,7 @@ res <- res[bool_significant,]
 
 df1 = setDT(res[,c(11,4)])[, .N, by = c(names(res[,c(11,4)]))]
 # Make the circular plot
-png(file='../Results/Plots/Fig2_CirclosPlot.png', width=600, height=600)
+pdf(file='../Results/Plots/Fig2_CirclosPlot.pdf', width=10, height=10)
 
 #chordDiagram(df1, transparency = 0.25, annotationTrack = c("grid"), preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df1))))), big.gap = 20, small.gap = 3)
 chordDiagram(df1, transparency = 0.25, annotationTrack = c("grid"), preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df1))))))
