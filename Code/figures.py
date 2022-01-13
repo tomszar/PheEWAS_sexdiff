7ec99c57f1afab1e81e9cb7848759ed6596a8c92
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2022-01-13 14:35:07 -0500

    miami and forest plots

--- Code/figures.py
+++ Code/figures.py
@@ -6,9 +6,15 @@ import matplotlib.pyplot as plt
 from nxviz import annotate
 
 
-def load_results():
+def load_results(type: str = 'all'):
     '''
     Load results table and return only significant associations
+
+    Parameters
+    ----------
+    type: str
+        the type of results requested, it can be 'all', 'significant',
+        'male', or 'female'
 
     Returns
     ----------
@@ -16,8 +22,15 @@ def load_results():
         results table
     '''
     results   = pd.read_csv('../Results/ResultsTable.csv')
-    keep_rows = ~(results['difference_type'] == 'None')
-    results   = results[keep_rows]
+    converged_columns = ['Converged_df',
+                         'Converged_rf',
+                         'Converged_dm',
+                         'Converged_rm']
+    converged = results.loc[:,converged_columns].all(axis=1)
+    results = results.loc[converged]
+    if type == 'significant':
+        keep_rows = ~(results['difference_type'] == 'None')
+        results   = results[keep_rows]
     return results
 
 def results_to_networkx(results):
@@ -296,3 +309,169 @@ def group_colormap(data: pd.Series):
             'MMA': b(17),
             'white blood cells': b(18)}
     return data.apply(lambda x: cmap.get(x))
+
+def plot_miami(results,
+               to_annotate: dict = None):
+    '''
+    Miami plot with female and male results.
+
+    Parameters
+    ----------
+    results: pd.DataFrame
+        results dataframe with male and female results
+    to_annotate: dict
+        dictionary with variable and outcome to annotate
+    '''
+    offset = 5  # Spacing between categories
+    df = results.copy()
+    
+    # Transform pvalues
+    df['log_pval_female'] = -np.log10(df['pvalue_female'])
+    df['log_pval_male'] = np.log10(df['pvalue_male'])
+
+    # Generating x position
+    df['category_x_offset'] = df.groupby('Variable_Category').ngroup() * offset
+    df['xpos'] = df.groupby(['Variable_Category', 'Variable_Name']).ngroup() + \
+                 df['category_x_offset']
+    
+    # X labels
+    x_labels = []
+    x_labels_pos = []
+
+    fig = plt.figure(figsize=(10,10))
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+
+    pval_columns = ['log_pval_female',
+                    'log_pval_male']
+    for category_num, (category_name, category_data) in enumerate(
+            df.groupby('Variable_Category') ):
+        if category_num % 2 == 0:
+            base_color = 'lightgray'
+        else:
+            base_color = 'darkgray'
+        left = category_data['xpos'].min() - (offset / 2) - 0.5
+        right = category_data['xpos'].max() + (offset / 2) + 0.5
+        width = right - left
+        x_labels.append(category_name)
+        x_labels_pos.append(left + width / 2)
+
+        for pval_col in pval_columns:
+            if pval_col == 'log_pval_female':
+                ax = ax1
+            else:
+                ax = ax2
+            ax.scatter(x='xpos',
+                       y=pval_col,
+                       label=None,
+                       color=base_color,
+                       s=30000 / len(df),
+                       data=category_data,
+                       alpha=0.6)
+            # Annotate
+            if to_annotate is not None:
+                for var in to_annotate:
+                    d = df.query('(Variable == @var) \
+                                  and Outcome == @to_annotate[@var]')
+                    x = d['xpos'].iloc[0]
+                    y = d[pval_col].max()
+                    name = d['Variable_Name'].iloc[0] + \
+                           '\n' + \
+                           d['Outcome_Name'].iloc[0]
+
+                    if var == 'LBXCOT':
+                        yoffset = 100
+                    elif var == 'SMD070':
+                        yoffset = 200
+                    elif var == 'SMD090':
+                        yoffset = 100
+                    else:
+                        yoffset = 150
+                    
+                    if pval_col == 'log_pval_female':
+                        xytext = (x, y + yoffset)
+                    else:
+                        xytext = (x, y - yoffset)
+                    ax.annotate(text=name,
+                                xy=(x,y),
+                                xytext=xytext,
+                                ha='left',
+                                arrowprops=dict(arrowstyle='->'))
+
+            # Highlight sex differences
+            for diff_num, (diff_name, diff_data) in enumerate(
+                category_data.groupby('difference_type')):
+                if diff_name == 'Pure':
+                    high_color = 'orange'
+                elif diff_name == 'Qualitative':
+                    high_color = 'green'
+                elif diff_name == 'Quantitative':
+                    high_color = 'blue'
+                
+                if diff_name != 'None':
+                    ax.scatter(x='xpos',
+                               y=pval_col,
+                               label=None,
+                               color=high_color,
+                               s=300000 / len(df),
+                               data=diff_data,
+                               alpha=0.7)
+
+    labels = df['Variable_Category'].unique()
+    for ax in [ax1, ax2]:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        if ax == ax1:
+            ax.set_xticks(x_labels_pos)
+            ax.set_xticklabels(x_labels, 
+                               rotation=45,
+                               ha='right',
+                               fontdict={'fontsize':8})
+        else:
+            ax.get_xaxis().set_visible(False)
+        #ax.spines['left'].set_visible(False)
+    
+    plt.tight_layout()
+    plt.savefig('../Results/Plots/Figure1.pdf')
+    plt.show()
+
+def plot_forest(results,
+                colors):
+    '''
+    Create a forest (dot) plot showing the effect sizes
+    between sexes
+
+    Parameters
+    ----------
+    results: pd.DataFrame
+        significant results to show
+    colors: matplotlib.colors.ListedColormap
+        colormap to use. Only takes the first two colors for female and male
+        in that order
+    '''
+    fig = plt.figure(figsize=(8,12))
+    ax = fig.add_subplot(111)
+    sexes = ['female',
+             'male']
+    differences = ['Pure',
+                   'Quantitative',
+                   'Qualitative']
+    df = results.sort_values(by=['difference_type',
+                                 'Variable',
+                                 'Outcome'])
+    y = list(range(0, len(df)))
+    beta_female = df.loc[:,'Beta_female']
+    beta_male = df.loc[:,'Beta_male']
+    ax.vlines([0], -1, 
+              len(df) + 1,
+              colors='k')
+    ax.plot(beta_female,
+            y,
+            'bo')
+    ax.plot(beta_male,
+            y,
+            'mo')
+    
+    plt.show()
+    
